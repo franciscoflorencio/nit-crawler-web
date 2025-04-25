59c95773f078fcdb6f730248b166333b6810cf4c
franciscoflorencio <franciscotaf@dcc.ufrj.br>
2025-04-25 13:53:33 -0300

    :sparkles: feat: CRUD de projects e articles funcionando para admin
    Agora o administrador do site pode adicionar projetos e artigos
    pertencentes ao grupo/ao NIT

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "corsheaders",
     "funding",
+    "articles",
+    "projects",
     "django_filters",
 ]
 
--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,14 +1,16 @@
 import React from "react";
-import { Container, Title, Content } from "./style";
+import { Container, Title, Content, Image } from "./style";
 
 interface CardProps {
   title: string;
   content: string;
+  image?: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, content }) => {
+const Card: React.FC<CardProps> = ({ title, content, image }) => {
   return (
     <Container>
+      {image && <Image src={image} alt={title} />}
       <Title>{title}</Title>
       <Content>{content}</Content>
     </Container>
