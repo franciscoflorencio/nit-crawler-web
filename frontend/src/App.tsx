b2ac2c0147746c6882f879db4f987e0c33b6a6c0
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2026-03-30 11:21:05 -0300

    remoção das paginas de crawl IA e Artigos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,10 +4,8 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Opportunities from "./pages/Opportunities";
 import { GlobalStyle } from "./GlobalStyle";
-import Articles from "./pages/Articles";
 import AboutUs from "./pages/About";
 import Contact from "./pages/Contact";
-import Crawlai from "./pages/CrawlAi";
 
 function App() {
   return (
@@ -19,8 +17,6 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/opportunities" element={<Opportunities />} />
-            <Route path="/crawlai" element={<Crawlai />} />
-            <Route path="/articles" element={<Articles />} />
             <Route path="/about" element={<AboutUs />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
